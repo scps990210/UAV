141beabe4e6638b9ad51214e6b4e8409933efcc0
scps990210 <66359433+scps990210@users.noreply.github.com>
2022-07-20 13:59:59 +0800

    Update positioning_d.m

--- positioning_d.m
+++ positioning_d.m
@@ -1,82 +1,93 @@
 viewer = siteviewer("Buildings","nycu.osm","Basemap","topographic");
 
-fileID = fopen('nycu2.txt','w');
+fileID = fopen('nycu6.txt','w');
 
-lat = [24.788088 , 24.788077, 24.788717, 24.788602];
-long = [120.999772, 121.000301, 121.000282, 121.000639];
-alt = [85,85,85];
-w = [0,0,0];
-frequency = 28000;
-weight = 0;
-lat_uav = 0;
-long_uav = 0;
+lat_uav = 24.787142;
+long_uav = 120.996546;
+alt = 105;
+flag = 1;
+count = 0;
+base = 0;
+threshold = 5 ;
 
-demandrate = [6,3,2];
-for i = 1 :1: 3
-    w(i) = demandrate(i);
-end
-for i = 1 :1: 3
-    weight = weight + w(i);
-end
-for i = 1 :1: 3
-    lat_uav = lat_uav + (w(i)/weight)*lat(i);
-    long_uav = long_uav + (w(i)/weight)*long(i);
-end 
+fq = 4.5e9;
+y = design(yagiUda,fq);
+y.Tilt = 180;
+y.TiltAxis = 'y';
 
 tx = txsite("Name","Small cell transmitter", ...
     "Latitude",lat_uav, ...
     "Longitude",long_uav, ...
     "AntennaHeight",40, ...
-    "TransmitterPower",5, ...
-    "TransmitterFrequency",28e9);
+    "TransmitterPower",0.25, ...
+    "TransmitterFrequency",fq);
 %show(tx)
 rtpm = propagationModel("raytracing", ...
     "MaxNumReflections",0, ...
     "BuildingsMaterial","perfect-reflector", ...
     "TerrainMaterial","perfect-reflector");
-coverage(tx,rtpm, ...
-    "SignalStrengths",-120:-5, ...
-    "MaxRange",250, ...
-    "Resolution",3, ...
-    "Transparency",0.6)
+%coverage(tx,rtpm, ...
+%    "SignalStrengths",-120:-5, ...
+%    "MaxRange",250, ...
+%    "Resolution",3, ...
+%    "Transparency",0.6)
 
-for i = 1 :1: 3
-    %get surface high
-    h = 0;
-    alt = 85;
-    rxs = rxsite("Name","Small cell receiver", ...
-        "Latitude",lat(i), ...
-        "Longitude",long(i), ...
-        "AntennaHeight",h);
-    surface = elevation(rxs)
-    h = alt - elevation(rxs)
-    %非建築區的信號強度
-    if h>=0
-        %(訊號接收站)
-        rxs = rxsite("Name","Small cell receiver", ...
-            "Latitude",lat(i), ...
-            "Longitude",long(i), ...
-            "AntennaHeight",h);
-        los(tx,rxs)
-        rtpm.MaxNumReflections = 1;
-        h2 = elevation(rxs);
-
-        rtpm.BuildingsMaterial = "concrete";
-        rtpm.TerrainMaterial = "concrete";
-        %raytrace(tx,rxs,rtpm)
-        ss = sigstrength(rxs,tx,rtpm);
-        %建築區標是為1
-    else
-        ss = 1;
+for lat=24.785342: 0.000009:24.788942
+    for lon=120.994746:0.000009:120.998346
+        dis = power(lat-lat_uav,2)+power(lon-long_uav,2);
+        if  dis > 0.00000324
+            flag = 0;
+        else 
+            flag = 1;
+        end
+            %get surface high
+            h = 0;
+            rxs = rxsite("Name","Small cell receiver", ...
+                 "Latitude",lat, ...
+                 "Longitude",lon, ...
+                 "AntennaHeight",h);
+            surface = elevation(rxs);
+            h = alt - elevation(rxs);
+            %非建築區的信號強度
+            if h>=0
+                %(訊號接收站)
+                rxs = rxsite("Name","Small cell receiver", ...
+                    "Latitude",lat, ...
+                    "Longitude",lon, ...
+                    "AntennaHeight",h);
+                los(tx,rxs)
+                rtpm.MaxNumReflections = 1;
+                h2 = elevation(rxs);
+                rtpm.BuildingsMaterial = "concrete";
+                rtpm.TerrainMaterial = "concrete";
+                %raytrace(tx,rxs,rtpm)
+                ss = sigstrength(rxs,tx,rtpm);
+                e = power(10,ss/10);
+                r = sinr(rxs,tx);
+            %建築區標是為1
+            else
+                ss = 1;
+            end
+            if (flag == 1) && (r > threshold)
+                count = count + 1 ;
+                base = base+1;
+            elseif(flag == 1) && (r < threshold)
+                base = base+1;
+            end
+            A=lat;
+            B=lon;
+            C=alt;
+            D=ss;
+            E=power(10,ss/10);
+            fprintf(fileID,'%12.8f\t',A);
+            fprintf(fileID,'%12.8f\t',B);
+            fprintf(fileID,'%12.8f\t',C);
+            fprintf(fileID,'%12.8f\t',D);
+            fprintf(fileID,'sinr = %12.8f\t',r);
+            fprintf(fileID,'count = %12.8f\t',count);
+            fprintf(fileID,'base = %12.8f\t',base);
+            fprintf(fileID,'percent = %12.8f\t',count/base);
+            fprintf(fileID,'sigstrength = %12.8f\n',E);
     end
-    A=lat(i);
-    B=long(i);
-    C=alt;
-    D=ss;
-    aa=[A;B;C;D]
-    fprintf(fileID,'%12.8f\t',A);
-    fprintf(fileID,'%12.8f\t',B);
-    fprintf(fileID,'%12.8f\t',C);
-    fprintf(fileID,'%12.8f\n',D);
 end
 fclose(fileID);
