121b75b84785c09ff815f9d9986cca6ae4be8bdb
scps990210 <66359433+scps990210@users.noreply.github.com>
2022-07-20 09:31:59 +0800

    Add files via upload

---
+++ height.m
@@ -0,0 +1,91 @@
+viewer = siteviewer("Buildings","nycu.osm","Basemap","topographic");
+
+fileID = fopen('nycu5.txt','w');
+
+lat_uav = 24.788314;
+long_uav = 121.000162;
+alt = 85;
+flag = 1;
+count = 0;
+base = 0;
+threshold = 0.00000530;
+
+fq = 4.5e9;
+y = design(yagiUda,fq);
+y.Tilt = 180;
+y.TiltAxis = 'y';
+
+tx = txsite("Name","Small cell transmitter", ...
+    "Latitude",lat_uav, ...
+    "Longitude",long_uav, ...
+    "AntennaHeight",40, ...
+    "TransmitterPower",0.25, ...
+    "TransmitterFrequency",fq);
+%show(tx)
+rtpm = propagationModel("raytracing", ...
+    "MaxNumReflections",0, ...
+    "BuildingsMaterial","perfect-reflector", ...
+    "TerrainMaterial","perfect-reflector");
+%coverage(tx,rtpm, ...
+%    "SignalStrengths",-120:-5, ...
+%    "MaxRange",250, ...
+%    "Resolution",3, ...
+%    "Transparency",0.6)
+
+for lat=24.788134: 0.000009:24.788494
+    for lon=120.999982:0.000009:121.000342
+        dis = power(lat-lat_uav,2)+power(lon-long_uav,2);
+        if  dis > 3.24e-8
+            flag = 0;
+        else 
+            flag = 1;
+        end
+            %get surface high
+            h = 0;
+            rxs = rxsite("Name","Small cell receiver", ...
+                 "Latitude",lat, ...
+                 "Longitude",lon, ...
+                 "AntennaHeight",h);
+            surface = elevation(rxs);
+            h = alt - elevation(rxs);
+            %非建築區的信號強度
+            if h>=0
+                %(訊號接收站)
+                rxs = rxsite("Name","Small cell receiver", ...
+                    "Latitude",lat, ...
+                    "Longitude",lon, ...
+                    "AntennaHeight",h);
+                los(tx,rxs)
+                rtpm.MaxNumReflections = 1;
+                h2 = elevation(rxs);
+                rtpm.BuildingsMaterial = "concrete";
+                rtpm.TerrainMaterial = "concrete";
+                %raytrace(tx,rxs,rtpm)
+                ss = sigstrength(rxs,tx,rtpm);
+                e = power(10,ss/10);
+            %建築區標是為1
+            else
+                ss = 1;
+            end
+            if (flag == 1) && (e > threshold)
+                count = count + 1 ;
+                base = base+1;
+            else
+                base = base+1;
+            end
+            A=lat;
+            B=lon;
+            C=alt;
+            D=ss;
+            E=power(10,ss/10);
+            fprintf(fileID,'%12.8f\t',A);
+            fprintf(fileID,'%12.8f\t',B);
+            fprintf(fileID,'%12.8f\t',C);
+            fprintf(fileID,'%12.8f\t',D);
+            fprintf(fileID,'count = %12.8f\t',count);
+            fprintf(fileID,'base = %12.8f\t',base);
+            fprintf(fileID,'percent = %12.8f\t',count/base);
+            fprintf(fileID,'sigstrength = %12.8f\n',E);
+    end
+end
+fclose(fileID);
